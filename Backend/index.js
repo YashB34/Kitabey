4938cdad783899d84ee72dc8a45b852a55c5ac29
YashB34 <yashkumar2115@gmail.com>
2025-11-17 08:31:09 +0530

    add env

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ app.use(cors({
 app.use(express.json());
 
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4001
 const URL = process.env.MONGO_URL
 // Connect to MongoDB
 try{
